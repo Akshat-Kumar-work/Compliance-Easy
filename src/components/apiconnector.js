916bbeda94a4a36c88838b28dff9269501cea085
Akshat <iamakku0.0.0.1@gmail.com>
2024-02-11 23:03:14 +0530

    updation

--- src/components/apiconnector.js
+++ src/components/apiconnector.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios"
 
 export const axiosInstance = axios.create({});
@@ -11,18 +10,4 @@ export const apiConnector = (method, url, bodyData, headers, params) => {
         headers: headers ? headers: null,
         params: params ? params : null,
     });
-=======
-import axios from "axios"
-
-export const axiosInstance = axios.create({});
-
-export const apiConnector = (method, url, bodyData, headers, params) => {
-    return axiosInstance({
-        method:`${method}`,
-        url:`${url}`,
-        data: bodyData ? bodyData : null,
-        headers: headers ? headers: null,
-        params: params ? params : null,
-    });
->>>>>>> 139dc3ee2dde2e6f01b5a9d249ed80e47b03803e
 }
